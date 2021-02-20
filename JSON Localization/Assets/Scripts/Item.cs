f20bdb73477a6d34cfeb2cf17788566d5bfac426
Simon Head <cuteorcsonline@protonmail.com>
2021-02-20 16:18:30 -0700

    Reformatted used namespace

--- JSON Localization/Assets/Scripts/Item.cs
+++ JSON Localization/Assets/Scripts/Item.cs
@@ -1,8 +1,10 @@
-﻿/// <summary>
+﻿using System;
+
+/// <summary>
 /// Represents a single row in a language's localization table.
 /// Each row is comprised of a key value and a text value (standard dictionary)
 /// </summary>
-[System.Serializable]
+[Serializable]
 public class Item
 {
     /// <summary>
