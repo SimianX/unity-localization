eec376e30c7a8e6db22aabcd87b9610e4f5b97e7
Simon Head <cuteorcsonline@protonmail.com>
2021-02-20 16:15:12 -0700

    Documented purpose of Item class

--- JSON Localization/Assets/Scripts/Item.cs
+++ JSON Localization/Assets/Scripts/Item.cs
@@ -1,4 +1,8 @@
-﻿[System.Serializable]
+﻿/// <summary>
+/// Represents a single row in a language's localization table.
+/// Each row is comprised of a key value and a text value (standard dictionary)
+/// </summary>
+[System.Serializable]
 public class Item
 {
     public string key;
