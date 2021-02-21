f1695a903d2d215bda8ee461570a3523ea56b967
Simon Head <cuteorcsonline@protonmail.com>
2021-02-21 16:05:37 -0700

    Implement language override button component

---
+++ JSON Localization/Assets/Scripts/LanguageOverrideButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class LanguageOverrideButton : MonoBehaviour, ILanguageOverrider
+{
+    [SerializeField]
+    private string languageCode;
+
+    /// <summary>
+    /// Accessor for implementing instance's currently selected language code.
+    /// Button selection is defined from the component's Unity editor field
+    /// </summary>
+    public string SelectedLanguageCode => languageCode;
+
+    /// <summary>
+    /// A method that overrides the application's language when given a supported language code.
+    /// Designed to easily attach to the button's componenent's On Click event
+    /// </summary>
+    public void TriggerOverride()
+    {
+        StartCoroutine(LocalizationManager.Instance.OverrideLanguage(SelectedLanguageCode));
+    }
+}
