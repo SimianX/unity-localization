6cc6db257dc992333815bfe7fd181b0387e03afe
Simon Head <cuteorcsonline@protonmail.com>
2021-02-21 19:09:17 -0700

    Renamed InitialLoadManager

---
+++ JSON Localization/Assets/Scripts/InitialLoadManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Responsible for validating a set of manager instances.
+/// Will transition to the next scene after all instances are flagged as ready
+/// </summary>
+public class InitialLoadManager : MonoBehaviour
+{
+    [SerializeField]
+    private string nextSceneName;
+
+    private IEnumerator Start()
+    {
+        while (!LocalizationManager.Instance.Ready)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
