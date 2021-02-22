f6bae9386f32ccb153cee2c7e521a7dc13db7a1d
Simon Head <cuteorcsonline@protonmail.com>
2021-02-22 13:36:09 -0700

    Created demo menu

---
+++ JSON Localization/Assets/Scripts/UI/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Generic UI methods to load scenes
+/// </summary>
+public class SceneController : MonoBehaviour
+{
+    /// <summary>
+    /// Loads a scene included in the project's build index
+    /// </summary>
+    /// <param name="sceneBuildIndex">
+    /// Index of the built scene
+    /// </param>
+    public void LoadScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+
+    /// <summary>
+    /// Loads a scene included in the project's build index
+    /// </summary>
+    /// <param name="sceneName">
+    /// Name of the built scene
+    /// </param>
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// Triggers Unity's Quit process
+    /// </summary>
+    public void TriggerQuit()
+    {
+        Application.Quit();
+    }
+}
